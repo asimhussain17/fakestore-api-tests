be16940a84dd7e08a8d76f5a25daf5d83d8c527d
Asim <asimakbar4@gmail.com>
2025-12-17 02:04:07 +0000

    small changes

--- tests/data/products.js
+++ tests/data/products.js
@@ -1,23 +1,23 @@
 export const clothingProducts = [
   {
-    title: 'Test T-Shirt',
+    title: 'White T-Shirt',
     price: 19.99,
     description: 'Comfortable cotton t-shirt',
-    image: 'https://i.pravatar.cc',
+    image: 'https://www.madeupproducts.co.uk/image.jpg',
     category: 'clothing'
   },
   {
     title: 'Test Hoodie',
     price: 39.99,
     description: 'Warm everyday hoodie',
-    image: 'https://i.pravatar.cc',
+    image: 'https://www.madeupproducts.co.uk/image.jpg',
     category: 'clothing'
   },
   {
     title: 'Test Cap',
     price: 14.99,
     description: 'Adjustable casual cap',
-    image: 'https://i.pravatar.cc',
+    image: 'https://www.madeupproducts.co.uk/image.jpg',
     category: 'clothing'
   }
 ];
